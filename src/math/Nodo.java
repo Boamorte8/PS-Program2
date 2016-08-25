3b2c924dc7a2eefcc8424116913f57b043ff9298
Esteban Salazar Zapata <Esteban Salazar Zapata>
2016-08-25 10:16:07 -0500

    Primer commit

---
+++ src/math/Nodo.java
@@ -0,0 +1,45 @@
+package math;
+
+public class Nodo {
+	private float dato1;
+	private float dato2;
+	private Nodo siguiente;
+	
+	@SuppressWarnings("null")
+	public Nodo(){
+		dato1 = (Float) null;
+		dato2 = (Float) null;
+		siguiente = null;
+	}
+	
+	public Nodo(float info1, float info2){
+		dato1 = info1;
+		dato2 = info2;
+		siguiente = null;
+	}
+	
+	public float getDato1() {
+		return dato1;
+	}
+
+	public void setDato1(float dato1) {
+		this.dato1 = dato1;
+	}
+
+	public float getDato2() {
+		return dato2;
+	}
+
+	public void setDato2(float dato2) {
+		this.dato2 = dato2;
+	}
+
+	public Nodo getSiguiente() {
+		return siguiente;
+	}
+
+	public void setSiguiente(Nodo siguiente) {
+		this.siguiente = siguiente;
+	}
+
+}
